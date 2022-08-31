db39f2fcd07f94c78eaefd10128d7bcae6cb5ecf
Chukwudi <ikenwakochukwudi@gmail.com>
2022-08-31 15:35:19 -0700

    refactored 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or not isinstance(a_dictionary, dict):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
     key_0 = list(a_dictionary.keys())[0]
