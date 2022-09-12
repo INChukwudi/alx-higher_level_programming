ca2f0205b18f3f0c59dd9cf5f1fecffba5c4c8e6
Chukwudi <ikenwakochukwudi@gmail.com>
2022-09-12 15:02:05 -0700

    added 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+
+elem = 0
+def safe_print_list(my_list=[], x=0):
+    for index in range(x):
+        try:
+            print(f"{my_list[index]}", end="")
+            elem += 1
+        except IndexError:
+            break
+    print("")
+    return (elem)
