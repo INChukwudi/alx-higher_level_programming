1d5671e91575f8404497d1f377fed920edd557b7
Chukwudi <ikenwakochukwudi@gmail.com>
2022-09-13 14:34:52 -0700

    added 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Defines a square based on 2-square.py
+"""
+
+
+class Square:
+    """
+    Represents the square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialises a new sqaure object
+
+        Args:
+            size (int): size of the sqaure
+        """
+        self.size = size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter for the size attribute
+        Validates the value to be set
+
+        Args:
+            value (int): new value of size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def size(self):
+        """
+        Gets and sets the value of the size attribute
+        """
+        return (self.__size)
+
+    def area(self):
+        """
+        Computes the area of the sqaure object
+        """
+        return (self.__size * self.__size)
