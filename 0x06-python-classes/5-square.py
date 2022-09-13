5174974df4c2c045cf3bf1e84da8af0dba3e0d0a
Chukwudi <ikenwakochukwudi@gmail.com>
2022-09-13 14:51:53 -0700

    added 5-sqaure.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Defines a square based on 4-square.py
+"""
+
+
+class Square:
+    """
+    Represents the square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialises a new sqaure object
+
+        Args:
+            size (int): size of the sqaure
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Gets and sets the value of the size attribute
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter for the size attribute
+        Validates the value to be set
+
+        Args:
+            value (int): new value of size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Computes the area of the sqaure object
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+        Prints in stdout the sqaure with the character #
+        """
+        for i in range(0, self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
