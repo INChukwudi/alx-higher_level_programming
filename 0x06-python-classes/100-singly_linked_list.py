4380ac778720c3c822723d900ad3511cacbc9761
Chukwudi <ikenwakochukwudi@gmail.com>
2022-09-13 23:27:25 -0700

    completed SinglyLinkedList implementation in 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -66,4 +66,43 @@ class SinglyLinkedList:
     """
     Defines a SinglyLinkedLIst instance
     """
-    pass
+    
+    def __init__(self):
+        """
+        Initialises a new SinglyLinnkedList instance
+        """
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """
+        Inserts a new node to the SinglyLinkeList
+        The node id inserted at the correct numerical ascending order
+
+        Args:
+            value (Node): data of the new node to be inserted
+        """
+        new_node = Node(value)
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new_node.next_node = tmp.next_node
+            tmp.next_node = new_node
+
+    def __str__(self):
+        """
+        Defines the `toString()` implementation of a SinglyLinkedList object
+        """
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ("\n".join(values))
