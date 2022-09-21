f2ea62e0b4bd1ca41c10287c9d00567e6d098790
Chukwudi <ikenwakochukwudi@gmail.com>
2022-09-20 22:21:22 -0700

    refactored 5*

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 
 """
 Defines a function that prints a text with 2 new lines after certain characters
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -20,9 +20,37 @@ class TestMaxInteger(unittest.TestCase):
         test_list = [1, 2, 3, 4, 5]
         self.assertEqual(max_integer(test_list), 5)
 
-     def test_unordered_list(self):
-         """
-         Test an unordered list of ints
-         """
-         test_list = [1, 5, 2, 4, 3]
-         self.assertEqual(max_integer(test_list), 55555)
+    def test_unordered_list(self):
+        """
+        Test an unordered list of ints
+        """
+        test_list = [1, 5, 2, 4, 3]
+        self.assertEqual(max_integer(test_list), 5)
+
+    def test_max_at_the_start(self):
+        """
+        Test a list with the max value at the start of the list
+        """
+        test_list = [5, 4, 3, 2, 1, 0]
+        self.assertEqual(max_integer(test_list), 5)
+
+    def test_empty_line(self):
+        """
+        Test an empty list
+        """
+        test_list = []
+        self.assertEqual(max_integer(test_list), None)
+
+    def test_single_element_list(self):
+        """
+        Test a list with a single element
+        """
+        test_list = [9]
+        self.assertEqual(max_integer(test_list), 9)
+
+    def test_ints_and_floats(self):
+        """
+        Test a list with ints and floats as its elements
+        """
+        test_list = [1.5, 2, 3.4, 5, 6.7]
+        self.assertEqual(max_integer(test_list), 6.7)
