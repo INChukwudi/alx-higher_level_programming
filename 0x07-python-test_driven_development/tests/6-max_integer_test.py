91835b4d7fe2d97aed535012e26385e83cddcb07
Chukwudi <ikenwakochukwudi@gmail.com>
2022-09-20 22:01:59 -0700

    added 6*

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,5 +17,12 @@ class TestMaxInteger(unittest.TestCase):
         """
         Test for an ordered list of ints
         """
-        list = [1, 2, 3, 4, 5]
-        self.assertEqual(max_integer(ordered), 5)
+        test_list = [1, 2, 3, 4, 5]
+        self.assertEqual(max_integer(test_list), 5)
+
+     def test_unordered_list(self):
+         """
+         Test an unordered list of ints
+         """
+         test_list = [1, 5, 2, 4, 3]
+         self.assertEqual(max_integer(test_list), 4)
