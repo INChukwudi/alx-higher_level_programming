0c5b83a2cafdff3e2071b360b9f550baa24eb746
Chukwudi <ikenwakochukwudi@gmail.com>
2022-09-27 21:40:51 -0700

    fixed styling issues in 100-append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,7 +7,7 @@ Defines a function that insertes text in a file after a specific string
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Inserta a line of text to a file after each line containing a specific string
+    Inserta a line of text to a file after each line containing search_string
 
     Args:
         filename (str): name of the file
