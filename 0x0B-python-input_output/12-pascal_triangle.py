cb232eafae0cada475fbfe838d18fa6ee02c601c
Chukwudi <ikenwakochukwudi@gmail.com>
2022-09-28 01:51:46 -0700

    finshed up pascal triangle functionality

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,3 +11,13 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
+
+    p_triangle = [[1]]
+    while len(p_triangle) != n:
+        c_triangle = p_triangle[-1]
+        tmp = [1]
+        for i in range(len(c_triangle) - 1):
+            tmp.append(c_triangle[i] + c_triangle[i + 1])
+        tmp.append(1)
+        p_triangle.append(tmp)
+    return p_triangle
