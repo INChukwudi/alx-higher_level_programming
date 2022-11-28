a54046e768cdebf5ddad60177923a9471c778f5c
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2022-11-28 22:27:01 +0000

    added mandatory task files

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// script that prints 3 lines using an array of string and a loop
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const line of lines) {
+  console.log(line);
+}
