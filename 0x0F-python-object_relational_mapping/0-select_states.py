1fe33a23cea303e9ac05549d372df579f446f065
Chukwudi <ikenwakochukwudi@gmail.com>
2022-12-04 13:45:36 -0800

    changed comment style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+
 
 import sys
 import MySQLdb
@@ -8,5 +11,5 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER By `id`")
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
     [print(state) for state in c.fetchall()]
