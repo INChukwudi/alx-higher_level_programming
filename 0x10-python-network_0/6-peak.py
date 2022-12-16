13f633ad9ce7ebe63efc1379719300468cd5c5c9
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2022-12-15 21:04:31 -0800

    added draft of task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Defines a function that finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Function that finds a peak in a list of unsorted integers
+    """
+
+    if list_of_integers == []:
+        return None
+
+    count = len(list_of_integers)
+    if count == 1:
+        return list_of_integers[0]
+    if count == 2:
+        return max(list_of_integers)
+
+    mid = int(count / 2)
+    peak = list_of_integers[mid]
+
+    return peak
